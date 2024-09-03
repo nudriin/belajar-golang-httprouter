246f8752445a18c613aa86ae0276a7f8a3a22aa2
Nurdin <mrsunnysummer@gmail.com>
2024-09-03 09:18:33 +0700

    Chapter 6 method not allowed

---
+++ ch6_method_not_allowed_handler/method_not_allowed_handler_test.go
@@ -0,0 +1,38 @@
+package ch6_method_not_allowed_handler
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMethodNotAllowedHandler(t *testing.T) {
+	router := httprouter.New()
+
+	// ! Membuat not found handler
+	// ! Apablia status code error 404 atau file tidak ketemu maka function ini akan di eksekusi
+	router.MethodNotAllowed = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Method ", r.Method, " Tidak di izinkan untuk handler ini")
+	})
+
+	router.GET("/halo", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		fmt.Fprint(w, "Hallo")
+	})
+
+	request := httptest.NewRequest("POST", "http://localhost:5000/halo", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Method POST Tidak di izinkan untuk handler ini", string(body))
+	fmt.Println(string(body))
+}
